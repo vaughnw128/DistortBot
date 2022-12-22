16b0a35d9fcc4f3072ab8b14417f91ce632ef4ce
Vaughn Woerpel <vww9966@rit.edu>
2022-12-21 21:35:26 -0500

    Added button functionality for continuous distortion

--- distort.py
+++ distort.py
@@ -58,6 +58,25 @@ async def grab_img(message):
 		
 		return fname
 
+async def distort(fname):
+	if fname is not None:
+		# Handles distorting the gif
+		if fname.endswith('gif'):
+			with Image(filename=fname) as temp_img:
+				with Image(width=round(temp_img.width*.60), height=round(temp_img.height*.60)) as new:
+					for _, frame in enumerate(temp_img.sequence):
+						frame.liquid_rescale(round(temp_img.width*.60), round(temp_img.height*.60))
+						new.sequence.append(frame)
+					new.type = 'optimize'
+					new.save(filename=fname)
+					return fname
+		# Handles distorting the photo
+		else:
+			with Image(filename=fname) as temp_img:
+				temp_img.liquid_rescale(round(temp_img.width*.60), round(temp_img.height*.60))
+				temp_img.save(filename=fname)
+				return fname
+
 # Defining the distortclient class with all of the command trees		
 class DistortClient(discord.Client):
 	def __init__(self, *args, **kwargs):
@@ -73,6 +92,15 @@ class DistortClient(discord.Client):
 		print(self.user.name)
 		print('------------')
 
+# View setup for the buttons
+class DistortView(discord.ui.View):
+	@discord.ui.button(label="Distort", style=discord.ButtonStyle.green)
+	async def distort_button_callback(self, interaction: discord.Interaction, button: discord.ui.Button):
+		fname = await grab_img(interaction.message)
+		distorted = await distort(fname)
+		await interaction.message.edit(attachments=[discord.File(distorted)])
+		await interaction.response.defer()
+
 # Defines the client and the client intents
 intents = discord.Intents.default()
 intents.message_content = True
@@ -87,23 +115,7 @@ async def ping(interaction: discord.Interaction):
 @client.tree.context_menu(name = "Distort", guild = MY_GUILD)
 async def distort_context_menu(interaction: discord.Interaction, message: discord.Message):
 	fname = await grab_img(message) # Grabs the image
-	print(fname)
-	if fname is not None:
-		# Handles distorting the gif
-		if fname.endswith('gif'):
-			with Image(filename=fname) as temp_img:
-				with Image(width=round(temp_img.width*.60), height=round(temp_img.height*.60)) as new:
-					for _, frame in enumerate(temp_img.sequence):
-						frame.liquid_rescale(round(temp_img.width*.60), round(temp_img.height*.60))
-						new.sequence.append(frame)
-					#new.type = 'optimize'
-					new.save(filename=fname)
-					await interaction.response.send_message(file=discord.File(fname))
-		# Handles distorting the photo
-		else:
-			with Image(filename=fname) as temp_img:
-				temp_img.liquid_rescale(round(temp_img.width*.60), round(temp_img.height*.60))
-				temp_img.save(filename=fname)
-				await interaction.response.send_message(file=discord.File(fname))
+	distorted = await distort(fname)
+	await interaction.response.send_message(file=discord.File(distorted), view=DistortView(timeout=None))
 
 client.run(token)
